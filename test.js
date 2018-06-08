1d4f1788b987ce77a38c041a6d0e2cf72fd88b11
Mark Oliver <mark.c.oliver@gmail.com>
2018-06-08 00:42:10 -0400

    second

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 // document.getElementById("toggle").addEventListener("click", toggleColor);
-
-export function toggleColor() {
+function toggleColor() {
   const el = document.getElementById("hello");
   el.setAttribute("class","cgreen")
 }
