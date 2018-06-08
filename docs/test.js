c877d7f679c082d54e1bfb6a1ec0ff406086b00b
invegat <mark.c.oliver@gmail.com>
2018-06-08 19:03:18 -0400

    docs

--- docs/test.js
+++ docs/test.js
@@ -1,15 +1,12 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
 // document.getElementById("toggle").addEventListener("click", toggleColor);
 // function toggleColor() {
 //   const el = document.getElementById("hello");
 //   const c = el.getAttribute("class");
 //   el.setAttribute("class", c === "cred" ? "cgreen" : "cred");
 // }
-var toggleColor = exports.toggleColor = function toggleColor() {
+var toggleColor = function toggleColor() {
   var el = document.getElementById("hello");
   var c = el.getAttribute("class");
   el.setAttribute("class", c === "cred" ? "cgreen" : "cred");
