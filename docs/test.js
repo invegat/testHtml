bd1f6a99c37b508f7968cfae1ae61e396a125600
invegat <mark.c.oliver@gmail.com>
2018-06-08 18:35:31 -0400

    docs

--- docs/test.js
+++ docs/test.js
@@ -1,6 +1,11 @@
 // document.getElementById("toggle").addEventListener("click", toggleColor);
-function toggleColor() {
+// function toggleColor() {
+//   const el = document.getElementById("hello");
+//   const c = el.getAttribute("class");
+//   el.setAttribute("class", c === "cred" ? "cgreen" : "cred");
+// }
+export const toggleColor = () => {
   const el = document.getElementById("hello");
   const c = el.getAttribute("class");
   el.setAttribute("class", c === "cred" ? "cgreen" : "cred");
-}
+};
