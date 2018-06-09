bfab597bd66968e19db083ae8c3cd71263328a8e
invegat <mark.c.oliver@gmail.com>
2018-06-08 20:30:43 -0400

    blue

--- docs/test.js
+++ docs/test.js
@@ -1,11 +1,7 @@
 "use strict";
 
 // document.getElementById("toggle").addEventListener("click", toggleColor);
-// function toggleColor() {
-//   const el = document.getElementById("hello");
-//   const c = el.getAttribute("class");
-//   el.setAttribute("class", c === "cred" ? "cgreen" : "cred");
-// }
+
 var toggleColor = function toggleColor() {
   var el = document.getElementById("hello");
   var c = el.getAttribute("class");
